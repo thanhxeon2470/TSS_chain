ec231cc2641a6331e514da8993bc9e9fe40c992d
Server UBT <thanhxeon2470@gmail.com>
2022-04-23 04:05:46 -0700

    update get balance

--- cli/cli_getbalance.go
+++ cli/cli_getbalance.go
@@ -14,16 +14,24 @@ func (cli *CLI) getBalance(address, nodeID string) {
 	}
 	bc := blockchain.NewBlockchain(nodeID)
 	UTXOSet := blockchain.UTXOSet{bc}
+	FTXSet := blockchain.FTXset{bc}
 	defer bc.DB.Close()
 
 	balance := 0
 	pubKeyHash := utils.Base58Decode([]byte(address))
 	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-4]
 	UTXOs := UTXOSet.FindUTXO(pubKeyHash)
+	FTXs := FTXSet.FindFTX(pubKeyHash)
 
 	for _, out := range UTXOs {
 		balance += out.Value
 	}
 
 	fmt.Printf("Balance of '%s': %d\n", address, balance)
+	if len(FTXs) > 0 {
+		fmt.Printf("List of file hash\n")
+	}
+	for i, link := range FTXs {
+		fmt.Printf("(%d) %s\n", i, link)
+	}
 }
