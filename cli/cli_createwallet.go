0c87ae685dc8124ff3f87d6d3189fef124754f02
Server UBT <thanhxeon2470@gmail.com>
2022-06-02 17:19:35 +0700

    added public key to get address

--- cli/cli_createwallet.go
+++ cli/cli_createwallet.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"fmt"
 
+	"github.com/thanhxeon2470/TSS_chain/utils"
 	"github.com/thanhxeon2470/TSS_chain/wallet"
 )
 
@@ -11,15 +12,16 @@ func (cli *CLI) CreateWallet() ([]byte, []byte, []byte) {
 	address := w.GetAddress()
 
 	prk := wallet.EncodePrivKey(w.PrivateKey)
-	pub := wallet.EncodePubkey(w.PublicKey)
+	pub := utils.Base58Encode(w.PublicKey)
 	fmt.Printf("Your TSS private key should be kept a secret. Whomever you share the private key with has access to spend all the bitcoins associated with that address.\n Your new address: %s\n Your private key: %s\n", address, prk)
 	return address, pub, prk
 }
 
-func (cli *CLI) AddWallet(priKey []byte) []byte {
+func (cli *CLI) AddWallet(priKey []byte) ([]byte, []byte) {
 	wallet := wallet.DecodePrivKey(priKey)
 	addr := wallet.GetAddress()
-
-	fmt.Printf("Your addres: %s\n", addr)
-	return addr
+	pub := utils.Base58Encode(wallet.PublicKey)
+	fmt.Printf("Your public key: %s\n", pub)
+	fmt.Printf("And addres: %s\n", addr)
+	return pub, addr
 }
