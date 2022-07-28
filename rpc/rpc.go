19b45196aab48790401b182140416edba65e0f6c
Server UBT <thanhxeon2470@gmail.com>
2022-07-28 23:56:13 +0700

    update gob for rpc

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -25,34 +25,27 @@ type Result struct {
 type RPC struct{}
 
 func (r *RPC) FindIPFS(args *Args, res *Result) error {
-	var buff bytes.Buffer
 	var payload findipfs
 
-	buff.Write(args.Req)
-	dec := gob.NewDecoder(&buff)
-	err := dec.Decode(&payload)
+	err := GobDecode(args.Req, &payload)
 	if err != nil {
 		return err
 	}
-
 	bc := blockchain.NewBlockchainView()
 	FTXSet := blockchain.FTXset{bc}
 	defer bc.DB.Close()
 	listUser := FTXSet.FindIPFS(payload.IpfsHashENC)
 	data := Ipfs{listUser}
-	res.Res = gobEncode(data)
-	return nil
+	res.Res, err = GobEncode(data)
+	return err
 }
 
 func (r *RPC) GetTxIns(args *Args, res *Result) error {
-	var buff bytes.Buffer
 	var payload gettxins
 
-	buff.Write(args.Req)
-	dec := gob.NewDecoder(&buff)
-	err := dec.Decode(&payload)
+	err := GobDecode(args.Req, &payload)
 	if err != nil {
-		log.Panic(err)
+		return err
 	}
 
 	address := payload.Addr
@@ -71,19 +64,16 @@ func (r *RPC) GetTxIns(args *Args, res *Result) error {
 	validOutputs := UTXOSet.FindAllSpendableOutputs(pubKeyHash)
 
 	data := Txins{validOutputs}
-	res.Res = gobEncode(data)
-	return nil
+	res.Res, err = GobEncode(data)
+	return err
 }
 
 func (r *RPC) GetBlance(args *Args, res *Result) error {
-	var buff bytes.Buffer
 	var payload getbalance
 
-	buff.Write(args.Req)
-	dec := gob.NewDecoder(&buff)
-	err := dec.Decode(&payload)
+	err := GobDecode(args.Req, &payload)
 	if err != nil {
-		log.Panic(err)
+		return err
 	}
 
 	address := payload.Addr
@@ -104,8 +94,8 @@ func (r *RPC) GetBlance(args *Args, res *Result) error {
 	for _, out := range UTXOs {
 		balance += out.Value
 	}
-	res.Res = gobEncode(Balance{balance, FTXs})
-	return nil
+	res.Res, err = GobEncode(Balance{balance, FTXs})
+	return err
 }
 
 func HandleRPC() {
@@ -152,14 +142,23 @@ type Fbproposal struct {
 	Accept bool
 }
 
-func gobEncode(data interface{}) []byte {
+func GobEncode(data interface{}) ([]byte, error) {
 	var buff bytes.Buffer
 
 	enc := gob.NewEncoder(&buff)
 	err := enc.Encode(data)
 	if err != nil {
-		log.Panic(err)
+		return nil, err
 	}
 
-	return buff.Bytes()
+	return buff.Bytes(), nil
+}
+
+func GobDecode(data []byte, payload interface{}) error {
+	dec := gob.NewDecoder(bytes.NewReader(data))
+	err := dec.Decode(payload)
+	if err != nil {
+		return err
+	}
+	return nil
 }
