4a9ff991d0e3f9b88f4e86b64e438c997fcea89d
thanhxeon2470 <57899554+thanhxeon2470@users.noreply.github.com>
2022-10-20 16:02:39 +0700

    Revert "Implemetation for RPC server (bitcoind 0.20.0)"

--- main.go
+++ main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"github.com/thanhxeon2470/TSS_chain/cli"
+
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
-	// fmt.Println("BAY DAU A")
-	// bc := blockchain.NewBlockchainView()
-	// fmt.Println("BAY DAU A")
-	// best , err := bc.GetLastBlock()
-	// if err != nil {
-	// 	fmt.Println("NHU CC", err)
-	// }
-	// fmt.Printf("AVASOFIJOAJW: +v" , best)
+
 	cli := cli.CLI{}
 	cli.Run()
 }
